ca3a59490e3f87bec083fd21ecbaf1e0faf299e7
Samuel Escobar <sescobar8714@gmail.com>
2020-02-16 02:31:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,7 @@ def reduce_to_total(source_array, starting_point=0)
       while i < source_array.count
        return false if source_array[i] = false
      else
-       source_array[i = true]
+       source_array[i] = true
        i += 1
       end
       return true
